e36bad6524cee7cc653c9b2b45590427697777c7
Noyaa5580 <timofeybarya2005@gmail.com>
2024-03-13 21:19:17 +0300

    idk (catalog page)

--- app/src/views/CatalogView.vue
+++ app/src/views/CatalogView.vue
@@ -1,5 +1,51 @@
 <template>
   <div>
-    <h1>This is an catalog page</h1>
+    <ProductCard
+          v-for="item in books_array"
+          :key="item"
+          :name="item.name"
+          :price="item.price"
+          :author="item.author"
+          :img_link="item.img_link"
+          :id="item.id"
+        ></ProductCard>
   </div>
 </template>
+<style>
+</style>
+
+<script>
+import ProductCard from "../components/ProductCard.vue";
+import axios from "axios";
+export default {
+  components: { ProductCard },
+  data: function () {
+    return {
+      books_array: [],
+      catalogs_array: ["bestsellers", "popular", "new-products"],
+      authors: [],
+
+    };
+  },
+  methods: {
+    randomize_authors() {
+      let random = Math.round(Math.random() * 2 - 0);
+      axios
+        .get("http://localhost:3000/" + this.catalogs_array[random])
+        .then((response) => (this.authors = response.data))
+        .catch((error) => {
+          console.error("Ошибка при запросе к API:", error);
+        });
+    },
+  },
+  mounted() {
+this.randomize_authors();
+    axios
+      .get("http://localhost:3000/cathalog")
+      .then((response) => (this.books_array = response.data))
+      .catch((error) => {
+        console.error("Ошибка при запросе к API:", error);
+      });
+  },
+};
+</script>
